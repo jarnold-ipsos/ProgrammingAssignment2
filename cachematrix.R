96eba971f6c56f29a890f2f68209f7a0e3c29ffc
Jack Arnold <Jack.Arnold@ipsos.com>
2022-03-11 15:01:43 -0800

    implemented cached matrix funcctionality

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## this function allows for a matrix's inverse to be cached
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse = NULL
+  set <- function(m) {
+    x <<- m
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(i) inverse <<- i
+  getInverse <- function() inverse
+  
+  list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
+## this function calls the cached matrix to calculate
+## an inverse, and store it, so that it is not recalculated
+## everytime
 cacheSolve <- function(x, ...) {
+  browser()
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message('This is cached')
+    return(inverse)
+  }
+  cache_matrix <- x$get()
+  inverse <- solve(cache_matrix, ...)
+  x$setInverse(inverse)
+  return(inverse)
         ## Return a matrix that is the inverse of 'x'
+}
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
